5c2650de2a2e047eac9faf31959e082d40984043
Ian Sutter <stormchaser30000@gmail.com>
2018-12-12 18:25:15 -0600

    more networking stuff

--- src/main.py
+++ src/main.py
@@ -1,28 +1,37 @@
-import _thread
+import threading
 import client, server
 
 #window = chat_window.ChatWindow()
 
 while True:
-    try:
-        print("Welcome to CypherChat")
-        print("Would you like to join or connect to a server? (j or c)")
-        option = input()
+    print("Welcome to CypherChat")
+    print("Would you like to join or create a server? (j or c)")
+    option = input()
 
-        if option == "j":
-            c = client.ChatClient()
+    if option == "j":
+        c = client.ChatClient()
 
-            print("Please enter the username that you would like to use\n:", end="")
-            username = input()
+        print("Please enter the username that you would like to use\n:", end="")
+        username = input()
 
-            print("Please enter the ip address you would like to connect to\n:", end="")
-            address = input()
-            c.connect(address, username)
-        else:
-            raise ValueError
-    except:
-        print("please enter c or j")
-        continue
+        print("Please enter the ip address you would like to connect to\n:", end="")
+        address = input()
+        c.connect(address, username)
+
+        while True:
+            message = input(":")
+            c.send_message(message, username)
+
+
+    elif option == "c":
+        s = server.ChatServer(threadID=1, name="Server")
+
+        username = input("Please enter a username:")
+        while True:
+            message = input(":")
+            s.send_message(message, username)
+    else:
+        print("Please enter c or j")
 
 
 
---
+++ src/server.py
@@ -0,0 +1,18 @@
+import socket, threading
+
+class ChatServer(threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+        self.server = socket.socket()
+        self.host = socket.gethostbyname(socket.gethostname())
+
+    def start_server(self, port):
+        self.server.bind(self.host, port)
+        self.server.listen()
+        self.server.accept()
+    
+    def send_message(self, message, username):
+        self.server.sendall("{}: {}".format(username, message))
