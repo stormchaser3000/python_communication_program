c9ae3e7a3c4bb459d2f9c0df62648d646ad565d0
Ian Sutter <stormchaser30000@gmail.com>
2018-12-12 20:48:59 -0600

    my brain must be broken to miss this

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ while True:
         print("Please enter the ip address you would like to connect to\n:", end="")
         address = input()
 
-        c = client.ChatClient(username, address)
+        c = client.ChatClient(1, "Client", username, address)
 
         while True:
             message = input()
